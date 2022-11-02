c66e2757783000bb4b5b51837ba3f1c761b78925
Ruhit Krishnan <68634796+ruhitkrishnan@users.noreply.github.com>
2022-11-02 22:15:23 +0530

    Add files via upload

---
+++ javascript/app.js
@@ -0,0 +1,42 @@
+// alert notice
+alert('vanakkam da mapla');
+
+
+// console queries
+console.log("hello world");
+console.log(4+4);
+console.log([1,2,3,4,5]);
+console.log({1:2,2:4});
+console.table({"apple":2,"mango":4});
+document.querySelector('h1').style.color='green';
+document.querySelector('body').style.backgroundColor='black';
+console.error("please enter valid information");
+console.warn("this is a warning message");
+console.log(Date());
+
+
+// type conversion
+val= 10;
+// val= String(Date());
+
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+console.log(val.toFixed(2));
+
+
+// string concartination
+const firstName='john';
+const lastName='Doe';
+
+Val= firstName+' '+lastName;
+console.log(Val);
+
+// escaping
+val='I\'m Tony\'s friend';
+console.log(val);
+//case
+val=firstName.toUpperCase();
+console.log(val);
+
+
